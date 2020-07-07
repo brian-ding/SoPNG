62109c63c0a0e6fd6a3e4b4cc61480c8510c25e3
Yuanpeng Ding <Brian_Ding@outlook.com>
2020-07-07 16:11:38 +0800

    feat: implement int to byte array

---
+++ PNGCore/Extensions/BitOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNGCore.Extensions
+{
+    public static class BitOperation
+    {
+        public static byte[] Int32ToBytes(int number)
+        {
+            byte[] result = new byte[4];
+            result[0] = (byte)(number << 0 >> 24);
+            result[1] = (byte)(number << 8 >> 24);
+            result[2] = (byte)(number << 16 >> 24);
+            result[3] = (byte)(number << 24 >> 24);
+
+            return result;
+        }
+    }
+}
